a8c6d846ba3d4d7976f76b795bcf0a2fd7255e2b
saadullah <154575515+saadullahkhan12@users.noreply.github.com>
2024-05-20 09:06:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+
+
+
+var main2 =document.getElementById("main2")
+
+var main1 =document.getElementById("main1")
+
+    
+
+function bold(){
+    main2.style.fontWeight="Bold"
+   
+}
+
+
+    
+function underline(){
+    main2.style.textDecoration="underline"
+  
+}
+function color(){
+    main2.style.color="blue"
+  
+}
+
+
+function color(){
+    main1.style.color="red"
+  
+}
+
